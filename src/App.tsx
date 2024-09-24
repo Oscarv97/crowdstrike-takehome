fa6abaa3606fa119e485f39a6dbcac04fe526db0
Oscar <oscarvial55@gmail.com>
2024-09-24 15:28:49 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
-      <h3 className="text-2xl font-semibold font-mono">Oscar Vial CrowdStrike</h3>
+      <h3 className="text-2xl font-semibold font-mono">Oscar Vial</h3>
       <div>
         <FilesPage />
         <ToastContainer />
